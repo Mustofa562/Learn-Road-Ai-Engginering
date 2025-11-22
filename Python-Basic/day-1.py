6e9d0640fb58e56321dca0831c80dab3f7270f31
Mutofa <mustofa9731@gmail.com>
2025-11-22 19:04:17 +0700

    Learn

---
+++ Python-Basic/day-1.py
@@ -0,0 +1,38 @@
+
+
+####    Belajar python   ####
+
+
+## Belajar data di dalam python
+
+a = 2       # ini adalah data Number(Integer)
+b = 'saya'  # ini adalah data Text (String)
+c = True    # ini adalah data Boolean (True/False)
+d = 2.1     # ini adalah data Angka Desimal (Float)
+
+# Ini adalah cara menampilkan varible
+print('manampilkan variable a', a)
+print('manampilkan variable b', b)
+print('manampilkan variable c', c)
+print('manampilkan variable d', d)
+
+# Catatan:
+# Fungsi varible itu seperti anda mempunyai kotak kosong
+# Kotak kosong yang anda buat, bisa di isi apapun yang anda inginkan
+# Dan variable bersifat menyimpan
+
+## Belajar Operator di dalam pyhton
+
+# (+) ini adalah tanda untuk operator plus
+# (-) ini adalah tanda untuk operator minus
+# (*) ini adalah tanda untuk operator kali
+# (/) ini adalah tanda untuk operator bagi
+
+# Contoh Operator
+print('ini adalah hasil operator plus(+) = ', 100 + 200 )
+print('ini adalah hasil operator minus(-) = ', 100 - 200 )
+print('ini adalah hasil operator Kali(*) = ', 100 * 200 )
+print('ini adalah hasil operator bagi(/) = ', 100 / 200 )
+
+# Catatan
+# Operator juga bisa digunakan untuk data string(Text)
